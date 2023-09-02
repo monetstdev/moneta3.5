0e56f9d9bdf218a44e923292e500b7d0f509353f
M.Tamura <tamura2004@gmail.com>
2023-09-02 20:45:29 +0900

    Update login page

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Moneta35
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.action_controller.allow_forgery_protection = false
   end
 end
